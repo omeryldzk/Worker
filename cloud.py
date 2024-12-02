7a839eabe1958017e54f69b21639731ee5fb26ed
ömer yıldız <yildizom20@itu.edu.tr>
2024-12-02 18:16:01 +0300

    port fixed

--- cloud.py
+++ cloud.py
@@ -93,5 +93,5 @@ def trigger_worker():
     worker()
     return jsonify({"message": "Worker triggered successfully"}), 200
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
